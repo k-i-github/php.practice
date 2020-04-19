a07b64067ce86ed1639e56f1b9ab33c859fbca80
kento <19922991kento@gmail.com>
2020-04-19 10:13:25 +0000

    04 kadai

---
+++ 04kadai.php
@@ -0,0 +1,52 @@
+<?php
+
+function sum($double) {
+    return $double * 2;
+    
+}
+
+echo sum(5);
+
+function kasan($a, $b) {
+    return $a + $b;
+}
+
+echo kasan(7,3);
+
+
+
+
+
+function hairetu($arr) {
+  $value= 1;
+  foreach($arr as $a) {
+   $value *= $a;
+  }
+  return $value;
+}
+
+
+hairetu(array(1, 3, 5 ,7, 9));
+ 
+echo hairetu(array(1, 3, 5 ,7, 9));
+
+
+
+ function max_array($arr){
+
+ $max_number = $arr[0];
+ foreach($arr as $a){
+     for($a = 1; $a < count($arr); $a++){
+         if($max_number < $arr[$a]){
+            $max_number = $arr[$a];
+         }
+     }
+ 
+ }
+
+ return $max_number;
+ }
+
+echo max_array(array(0, 6, 3,200, 11));
+
+?>
